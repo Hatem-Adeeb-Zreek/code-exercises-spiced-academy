143af4bf23f6cf3d3542c6a1f7209ffe9d820d35
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-11 20:20:11 +0100

    exercise1

---
+++ week1/functions-and-scope/exercise1.js
@@ -0,0 +1,11 @@
+function sum() {
+    var result = 0;
+    for (i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+    return result;
+}
+
+sum(5, 10);
+sum(5, 10, 15);
+sum(5, 10, 15, 100, 200);
