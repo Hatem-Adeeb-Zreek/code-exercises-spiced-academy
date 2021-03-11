a2b8f86cf55377d69f0045cc77101a828267005c
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-11 20:23:32 +0100

    fix a bug inside exercise1.js

--- week1/functions-and-scope/exercise1.js
+++ week1/functions-and-scope/exercise1.js
@@ -1,6 +1,6 @@
 function sum() {
     var result = 0;
-    for (i = 0; i < arguments.length; i++) {
+    for (var i = 0; i < arguments.length; i++) {
         result += arguments[i];
     }
     return result;
