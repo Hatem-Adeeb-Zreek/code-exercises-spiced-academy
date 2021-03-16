b10aaf6e36fdc3c06646d0dfc97741164cc1d023
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-16 13:07:16 +0100

    exercise1.js

---
+++ week2/DOM/exercise1.js
@@ -0,0 +1,11 @@
+function targetElementsBySelector(selector) {
+    var targedElements = document.querySelectorAll(selector);
+
+    if (targedElements) {
+        for (var i = 0; i < targedElements.length; i++) {
+            targedElements[i].style.fontStyle = "italic";
+            targedElements[i].style.textDecoration = "underline";
+            targedElements[i].style.fontWeight = "bold";
+        }
+    }
+}
