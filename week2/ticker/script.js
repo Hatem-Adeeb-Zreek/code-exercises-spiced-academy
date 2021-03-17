e7d69e3c68440e5efe279f7c317049c59fa35e68
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-17 22:17:46 +0100

    ticker part 2

--- week2/ticker/script.js
+++ week2/ticker/script.js
@@ -2,15 +2,29 @@
     var headings = document.getElementById("headlines");
     var links = document.getElementsByTagName("A");
     var leftPosition = headings.offsetLeft;
+    var reqId;
 
     var animationFunction = function () {
         leftPosition--;
-        if (leftPosition <= -links[0].offsetWidth) {
+        if (leftPosition <= 0 - links[0].offsetWidth) {
             leftPosition = leftPosition + links[0].offsetWidth;
             headings.appendChild(links[0]);
         }
         headings.style.left = leftPosition + "px";
-        requestAnimationFrame(animationFunction);
+        reqId = requestAnimationFrame(animationFunction);
     };
     animationFunction();
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("mouseenter", function (e) {
+            cancelAnimationFrame(reqId);
+            e.target.style.textDecoration = "underline";
+            e.target.style.color = "blue";
+        });
+        links[i].addEventListener("mouseleave", function (e) {
+            e.target.style.color = "rgb(163, 7, 7)";
+            e.target.style.textDecoration = "none";
+            animationFunction();
+        });
+    }
 })();
