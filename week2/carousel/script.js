a1e2ffa78d44052f389613d590f13cb7f2bf20e0
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-22 21:46:35 +0100

    carousel part-2

--- week2/carousel/script.js
+++ week2/carousel/script.js
@@ -1,27 +1,52 @@
 (function () {
     var kitties = document.querySelectorAll("img");
+    var dots = document.getElementsByClassName("dot");
     var currentKitty = 0;
+    var timer;
+    var checkTransitioning;
 
-    function moveKitties() {
-        kitties[currentKitty].classList.remove("onscreen");
-        kitties[currentKitty].classList.add("exit");
-        currentKitty++;
-
-        if (currentKitty === kitties.length) {
-            currentKitty = 0;
-        }
-        kitties[currentKitty].classList.add("onscreen");
-        setTimeout(moveKitties, 4000);
+    for (var i = 0; i < dots.length; i++) {
+        (function (i) {
+            dots[i].addEventListener("click", function (e) {
+                if (e.target.classList.contains("on")) {
+                    return;
+                }
+                if (checkTransitioning) {
+                    return;
+                }
+                clearTimeout(timer);
+                moveKitties(i);
+            });
+        })(i);
     }
 
-    setTimeout(function () {
-        kitties[currentKitty].classList.add("onscreen"); // the problem of showing the first image and wait 4s then other images solved
-        setTimeout(function () {
-            moveKitties();
-        }, 4000);
-    }, 4000);
-
     document.addEventListener("transitionend", function (e) {
-        e.target.classList.remove("exit");
+        if (e.target.classList.contains("exit")) {
+            e.target.classList.remove("exit");
+            timer = setTimeout(moveKitties, 4000);
+            checkTransitioning = false;
+        }
     });
+
+    timer = setTimeout(moveKitties, 4000);
+
+    function moveKitties(next) {
+        checkTransitioning = true;
+        kitties[currentKitty].classList.remove("onscreen");
+        kitties[currentKitty].classList.add("exit");
+        dots[currentKitty].classList.remove("on");
+        if (typeof next != "undefined") {
+            currentKitty = next;
+        } else {
+            currentKitty++;
+            if (currentKitty === kitties.length) {
+                console.log("currentKitty", typeof currentKitty);
+                console.log("kitties.length", typeof kitties.length);
+                currentKitty = 0;
+            }
+        }
+
+        kitties[currentKitty].classList.add("onscreen");
+        dots[currentKitty].classList.add("on");
+    }
 })();
