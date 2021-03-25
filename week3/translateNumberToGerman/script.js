931db25b4dca46754506128fc6b60af7b06fdb3a
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-26 00:32:35 +0100

    numbers in german

---
+++ week3/translateNumberToGerman/script.js
@@ -0,0 +1,31 @@
+function numbersInGerman() {
+    var arr = [
+        "eins",
+        "zwei",
+        "drei",
+        "vier",
+        "fünf",
+        "sechs",
+        "sieben",
+        "acht",
+        "neun",
+        "zehn",
+    ];
+    try {
+        var parsedNumber = parseInt(askForNumber());
+        console.log(arr[parsedNumber - 1]);
+    } catch (error) {
+        console.log(error.message);
+        numbersInGerman();
+    }
+}
+
+function askForNumber() {
+    var num = prompt("Please enter a number between 1 and 10");
+    if (num >= 1 && num <= 10 && num == parseInt(num)) {
+        return num;
+    }
+    throw new Error("Bad number");
+}
+
+numbersInGerman();
