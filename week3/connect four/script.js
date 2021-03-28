22e754df80f8c0814b499a3f05f5e5045aea012f
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-28 12:20:51 +0200

    add clickEventListener for the cols and write the diagonalWinning function

--- week3/connect four/script.js
+++ week3/connect four/script.js
@@ -2,11 +2,38 @@
     // target elements and declare variables
     var board = $(".board");
     var column = "";
-    var slots;
+    var slots = board.find(".slot");
+    var col = $(".column");
+    var currentPlayer = "player1";
+    var diagonalWinArray = [
+        [38, 33, 28, 23],
+        [32, 27, 22, 17],
+        [37, 32, 27, 22],
+        [26, 21, 16, 11],
+        [36, 31, 26, 21],
+        [20, 15, 10, 5],
+        [25, 20, 15, 10],
+        [30, 25, 20, 15],
+        [19, 14, 9, 4],
+        [24, 19, 14, 9],
+        [18, 25, 32, 39],
+        [19, 26, 33, 40],
+        [12, 19, 26, 33],
+        [20, 27, 34, 41],
+        [13, 20, 27, 34],
+        [6, 13, 20, 27],
+        [2, 9, 16, 23],
+        [8, 15, 22, 29],
+        [1, 8, 15, 22],
+        [14, 21, 28, 35],
+        [7, 14, 21, 28],
+        [0, 7, 14, 21],
+    ];
 
     // call createSlots function
     createSlots();
 
+    // functions
     function createSlots() {
         // create one column and loop throw six rows
         column += "<div class='column'>";
@@ -20,4 +47,22 @@
             board.append(column);
         }
     }
+    function diagonalWinning() {
+        // loop throw diagonal array to check the winning
+        for (var i = 0; i < diagonalWinArray.length; i++) {
+            if (
+                slots.eq(diagonalWinArray[i][0]).hasClass(currentPlayer) &&
+                slots.eq(diagonalWinArray[i][1]).hasClass(currentPlayer) &&
+                slots.eq(diagonalWinArray[i][2]).hasClass(currentPlayer) &&
+                slots.eq(diagonalWinArray[i][3]).hasClass(currentPlayer)
+            ) {
+                return true;
+            }
+        }
+    }
+
+    // add click event for the columns
+    col.on("click", function () {
+        diagonalWinning();
+    });
 })();
