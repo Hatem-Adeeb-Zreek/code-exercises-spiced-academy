db547bc86059d8a6a2a3ac6c5a2aaf9a8d267d69
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-03-29 20:16:50 +0200

    fix a buggit status!

--- week3/ticker jQuery version/script.js
+++ week3/ticker jQuery version/script.js
@@ -1,35 +1,34 @@
-(function () {
+(function fn() {
     var headings = $("#headlines");
     var links = $("a");
-    console.log(links);
-    var leftPosition = headings.offset().left;
-    var reqId;
+    var left = headings.offset().left;
+    var id;
 
     var animationFunction = function () {
-        leftPosition--;
-        if (leftPosition <= 0 - links.eq(0).width()) {
-            leftPosition = leftPosition + links.eq(0).width();
+        links = $("a");
+        left--;
+        if (left <= 0 - links.eq(0).outerWidth()) {
+            left += links.eq(0).outerWidth();
             headings.append(links.eq(0));
         }
         headings.css({
-            left: leftPosition + "px",
+            left: left + "px",
         });
-        reqId = requestAnimationFrame(animationFunction);
+        id = requestAnimationFrame(animationFunction);
     };
     animationFunction();
 
     for (var i = 0; i < links.length; i++) {
         links.eq(i).on("mouseenter", function (e) {
-            cancelAnimationFrame(reqId);
+            cancelAnimationFrame(id);
             $(e.target).css({
                 textDecoration: "underline",
                 color: "blue",
             });
         });
-
         links.eq(i).on("mouseleave", function (e) {
             $(e.target).css({
-                color: "rgb(163, 7, 7)",
+                color: "",
                 textDecoration: "none",
             });
             animationFunction();
