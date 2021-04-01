cd5da4a04fc4c1d9608a69b041497f175eb1347f
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-04-01 16:13:43 +0200

    no slice but with spread operator

--- week4/thursday exercises/ex1.js
+++ week4/thursday exercises/ex1.js
@@ -2,7 +2,7 @@
 //containing all of the items that are in the array that was passed in but in reverse order.
 
 (function () {
-    const reversedArray = (arr) => arr.slice().reverse();
+    const reversedArray = (arr) => [...arr].reverse();
     let numbers = [1, 2, 3, 4, 5];
     console.log(reversedArray(numbers));
     console.log(numbers);
