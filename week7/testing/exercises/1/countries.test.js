cf017527fec51000a8e1ea3b63655efef6234535
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-04-19 20:40:33 +0200

    jest update ex1

--- week7/testing/exercises/1/countries.test.js
+++ week7/testing/exercises/1/countries.test.js
@@ -11,8 +11,8 @@ test("to make sure the array has no more than 4 results", () => {
 });
 
 test("the search is case insensitive", () => {
-    const result = countries.find("gErMaNy");
-    expect(result[0]).toBe("Germany");
+    const result = countries.find("SyRiA");
+    expect(result[0]).toBe("Syria");
 });
 
 test("no matching countries ==> return an empty array", () => {
