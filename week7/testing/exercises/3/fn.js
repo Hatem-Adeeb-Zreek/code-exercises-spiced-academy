5a1d62d80daba1f95030d6aecd91770a24614203
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-04-19 20:52:53 +0200

    jest exe 3

--- week7/testing/exercises/3/fn.js
+++ week7/testing/exercises/3/fn.js
@@ -1,2 +1,13 @@
-module.exports = function fn() {
+module.exports = function fn(arg) {
+    if (typeof arg === "string") {
+        return arg === "" ? "" : fn(arg.substr(1)) + arg.charAt(0);
+    } else if (typeof arg !== "string" && !Array.isArray(arg)) {
+        return null;
+    } else if (Array.isArray(arg)) {
+        let array = [];
+        for (let i = 0; i < arg.length; i++) {
+            array.push(fn(arg[i]));
+        }
+        return array;
+    }
 };
